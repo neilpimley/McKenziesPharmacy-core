0901e6da8d513be9a6860c84ee4d4b430a9ffe73
ORWELLG\n.pimley <n.pimley@orwellg.com>
2018-02-06 14:41:27 +0000

    bat file updated with dotnet commands

--- build.bat
+++ build.bat
@@ -15,16 +15,8 @@ if "%nuget%" == "" (
 )
 
 REM Build
-dotnet publish --configuration "%config%"
+dotnet build --configuration "%config%"
 
-
-REM Package
-mkdir Build
-mkdir Build\lib
-mkdir Build\lib\netcoreapp2.0
-
-copy pharmacy.models\bin\%config%\netcoreapp2.0\pharmacymodels.dll Build\lib\netcoreapp2.0
-copy pharmacy.repositories\bin\%config%\netcoreapp2.0\pharmacyrepositories.dll Build\lib\netcoreapp2.0
-copy pharmacy.services\bin\%config%\netcoreapp2.0\pharmacyservices.dll Build\lib\netcoreapp2.0
-
-call %nuget% pack "mckenzies.pharmacy.core.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+dotnet pack pharmacy.models/pharmacy.models.csproj --configuration "%config%"
+dotnet pack pharmacy.repositories/pharmacy.repositories.csproj --configuration "%config%"
+dotnet pack pharmacy.services/pharmacy.services.csproj --configuration "%config%"
